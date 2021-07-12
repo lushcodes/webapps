f7a3d245e07bd9683bffa9d4bde8b11993c1fbc2
lushcodes <lushcodes@gmail.com>
2021-07-12 20:50:38 +0100

    Added sinatra reloader & cat picture

--- web/webapp.rb
+++ web/webapp.rb
@@ -6,5 +6,11 @@ get '/' do
 end
 
 get '/secret' do
-  'This is the secret path'
+  'This is the secret path to heaven'
+end
+
+get '/cat' do
+"<div style ='border: 3px dashed red'>
+<img src='http://placekitten.com/500/500'>
+ </div>"
 end
